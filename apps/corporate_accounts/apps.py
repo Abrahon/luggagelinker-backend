c7d97a5eefacae8dbe1e4bcf17d3f3c67dbb123a
Sujon <mdabrahon926@gmail.com>
2026-06-26 10:29:30 +0600

    added project setup and pg

---
+++ apps/corporate_accounts/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CorporateAccountsConfig(AppConfig):
+    name = 'corporate_accounts'
---
+++ apps/saved_routes/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SavedRoutesConfig(AppConfig):
+    name = 'saved_routes'
