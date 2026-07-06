1b3d81a51756a3bda41698ec584aae58c27644be
Sujon <mdabrahon926@gmail.com>
2026-07-06 17:03:07 +0600

    new field added

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -86,6 +86,10 @@ class Profile(models.Model):
         default=0.00,
         help_text="Average traveler rating.",
     )
+    completed_deliveries = models.PositiveIntegerField(default=0)
+
+    cancelled_deliveries = models.PositiveIntegerField(default=0)
+
 
     city = models.CharField(
         max_length=100,
